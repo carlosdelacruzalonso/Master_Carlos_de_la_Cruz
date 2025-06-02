fa46e26459e1552f23b5e17d85925044071b0d4c
Carlos <carlosdelacruzalonso@gmail.com>
2025-06-02 20:54:34 +0200

    Segunda clase de javascript

---
+++ javascript/2.Clase_02_06.js
@@ -0,0 +1,53 @@
+// Operadores
+// Simbolos reservados para ciertas operaciones
+
+// Aritméticos
+
+= operador de asignación 
+
+let x = 10;
+x = x + 5;
+console.log(x);
+
++ - * / % 
+
+suma, resta, multipliación, división y resto
+
+resto sirve para saber si un numero es par o impar
+
+si resto es 0 es par, si resto es distinto de 0 es impar
+
+
+x = x + 5; ----> forma rápida  ----->  x+=5
+
+x++; operador de incremetar 1
+x--; operdador de decrecer 1
+
+método Math
+
+// Lógicos
+
+ ||  ->  O  ->  OR
+
+ &&  ->  Y  -> AND
+
+ !   ->  NO -> NOT
+
+ ==  -> IGUAL QUE
+
+ === -> ESTRICTAMENTE IGUAL QUE
+
+ !=  -> DESIGUAL
+
+ !== -> ESTRICTAMENTE DESIGUAL QUE
+
+  =  -> OPERADOR DE ASIGNACIÓN
+
+  <  -> MENOR QUE
+
+  <= -> MENOR O IGUAL QUE
+
+  >  -> MAYOR QUE
+
+  >= -> MAYOR O IGUAL QUE
+
