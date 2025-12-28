b562b0ca9fd0d022520295d72df06f55d3024534
Carlos <carlosdelacruzalonso@gmail.com>
2025-12-28 20:29:38 +0100

    Delete proyecto2-katas-javascript directory

--- proyecto2-katas-javascript/ejercicios-es6-asincronia/ejercicio9.js
+++
@@ -1,9 +0,0 @@
-let img = document.querySelector('.random-image')
-
-let randomNum = Math.floor(Math.random() * 151) + 1
-
-fetch('https://pokeapi.co/api/v2/pokemon/' + randomNum)
-    .then(res => res.json())
-    .then(data => {
-        img.src = data.sprites.other['official-artwork'].front_default
-    })
